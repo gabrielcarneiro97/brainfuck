a567eaf5c16d3e2e1073cda0269f193e75aaad0d
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-03-05 15:15:41 -0300

    Agora tem em C (incompleto)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# brainfuck-js
-A interpreter of the worst language ever (brainfuck) written in the second worst, also know as javascript.
+# brainfuck
+Interpretadores de brainfuck em algumas linguagens legais, menos Js, Js é uma merda.
+
+## JS
+Já tá tudo implementado, menos a função de leitura que vai dar um pouco mais de trabalho.
+
+## C
+Os loops ainda não estão implementados.
---
+++ main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+char string[] = ",.+++.";
+
+void moveRight (int ** now) {
+  *now = *now + 1;
+}
+
+void moveLeft(int ** now) {
+  *now = *now - 1;
+}
+
+void increment (int * now){
+  *now = *now + 1;
+}
+
+void decrement(int * now) {
+  *now = *now - 1;
+}
+
+void print (int * now) {
+  printf("%d\n", *now);
+}
+void read (int * now) {
+  char c;
+  scanf("%c", &c);
+  *now = (int) c;
+}
+
+void doIt (char string[], int charId, int * now) {
+  char charNow = string[charId];
+
+  switch (charNow) {
+    case '+':
+      increment(now);
+      break;
+    case '-':
+      decrement(now);
+      break;
+    case '>':
+      moveRight(&now);
+      break;
+    case '<':
+      moveLeft(&now);
+      break;
+    case '.':
+      print(now);
+      break;
+    case ',':
+      read(now);
+      break;
+    default:
+      break;
+  }
+}
+
+int main() {
+
+  int stringLength = sizeof(string) / sizeof(char);
+
+  int dataArray[30000];
+
+  int * now = dataArray;
+
+  int charId;
+  for (charId = 0; charId < stringLength; charId++) {
+    char ch = string[charId];
+    doIt(string, charId, now);
+  }
+}
